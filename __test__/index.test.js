6cf527c7173142bce86361422e8d71009fd95804
Ladan Torkzaban <99889241+ladantork@users.noreply.github.com>
2024-03-18 15:05:32 +0000

    Sum digits remain

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,13 +1,24 @@
 const reversStr = require ('../reverseString.js')
+const sumDigit = require('../sum.js')
 
 describe('reversStr',()=>{
     test('returns the same letter ',()=>{
-     //expect(reversStr("r")).toBe("r")
+     expect(reversStr("r")).toBe("r")
      expect(reversStr("hello")).toBe("olleh")
-     //expect(reversStr("ra")).toBe("ar")
-     //expect(reversStr("katherine rules")).toBe("selur enirehtak")
+     expect(reversStr("ra")).toBe("ar")
+     expect(reversStr("katherine rules")).toBe("selur enirehtak")
     })
+})
 
-
+    describe('sumDigits',()=>{
+        test('return that digit',()=>{
+         expect(sumDigit(1)).toBe(1)
+        })
+        test('return sum of two digits',()=>{
+            expect(sumDigit(11)).toBe(2)
+           })
+           test.only('return sum of digits',()=>{
+            expect(sumDigit(99)).toBe(9)
+           })
 
 })
